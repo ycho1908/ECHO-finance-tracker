14e16cb348d633a62098a78faf3ef7fb31686a5a
Evelyn Cho <yehyeoncho@gmail.com>
2024-09-17 23:50:46 -0700

    updated contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,9 +7,9 @@ function Contact() {
   return (
     <div>
       <p>
-        Here is my personal page! <a href="https://ycho1908.github.io/personal_page/" style={{ color: 'white', textDecoration: 'underline' }}> https://ycho1908.github.io/personal_page/ </a>
+        {/* Here is my personal page! <a href="https://ycho1908.github.io/personal_page/" style={{ color: 'white', textDecoration: 'underline' }}> https://ycho1908.github.io/personal_page/ </a> */}
         <br/>
-        Feel free to contact me through email or LinkedIn and check out my github!
+        {/* Feel free to contact me through email or LinkedIn and check out my github! */}
         <br/>
       </p>
       <Spline scene="https://prod.spline.design/fg-Sy3mBniEBiZVL/scene.splinecode" />
