b8d8b89ea4fdd3bd5cbc2e4d3b65c6febb5746da
Evelyn Cho <yehyeoncho@gmail.com>
2024-09-18 00:08:38 -0700

    updated the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,14 +6,8 @@ import { Analytics } from '@vercel/analytics/react';
 function Contact() {
   return (
     <div>
-      <p>
-        {/* Here is my personal page! <a href="https://ycho1908.github.io/personal_page/" style={{ color: 'white', textDecoration: 'underline' }}> https://ycho1908.github.io/personal_page/ </a> */}
-        <br/>
-        {/* Feel free to contact me through email or LinkedIn and check out my github! */}
-        <br/>
-      </p>
       <Spline scene="https://prod.spline.design/fg-Sy3mBniEBiZVL/scene.splinecode" />
-    <Analytics/>
+      <Analytics/>
     </div>
   );
 }
